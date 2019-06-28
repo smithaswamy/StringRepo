84d411d477d7dbfc033d2dc0c16d61dfcd86f488
097034 <smitha.swamy@nttdata.com>
2019-06-28 13:33:12 -0500

    firstcommit

---
+++ SimpleProject/src/com/demo/StringDemo.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+	
+		String s1="Hello";
+		String s2="World";
+		if(s1.equals(s2))
+			System.out.println("String identical");
+		else
+			System.out.println("Strings not identical");
+	}
+
+}
